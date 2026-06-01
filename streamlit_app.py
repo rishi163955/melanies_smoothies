ea74ffb4650eab5f7e9275c877198c41a9b2f541
rishi163955 <rishi.s@mastek.com>
2026-06-01 15:52:37 +0530

    Refactor ingredient handling and API call logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,17 +65,21 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = " ".join(ingredients_list)
-
+    # ingredients_string = " ".join(ingredients_list)
+    for fruit_chosen in ingredients_list:
+        ingredient_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen +" Nutrition Infomation")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        
     my_insert_stmt = (
         "insert into smoothies.public.orders (ingredients, name_on_order) "
         "values ('" + ingredients_string + "', '" + name_on_order + "')"
     )
 
     time_to_insert = st.button("Submit Order")
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    # st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+    
     if time_to_insert:
         try:
             session.sql(my_insert_stmt).collect()
