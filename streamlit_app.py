ef4cdff0cc340e0b613fafa3d349b761d6188926
rishi163955 <rishi.s@mastek.com>
2026-06-01 19:44:04 +0530

    Refactor fruit options retrieval in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,13 +59,6 @@ name_on_order = st.text_input("Name on smoothie:")
 st.write("Name on your smoothie will be:")
 st.write(name_on_order)
 
-my_df = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
-pd_df = my_df.to_pandas()
-st.dataframe(pd_df)
-# st.stop()
-search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
 ingredients_list = st.multiselect(
     "Choose up to 5 fruits",
     fruit_list,
@@ -80,6 +73,13 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
         # st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+        my_df = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
+        pd_df = my_df.to_pandas()
+        st.dataframe(pd_df)
+        # st.stop()
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
     my_insert_stmt = (
         "insert into smoothies.public.orders (ingredients, name_on_order) "
