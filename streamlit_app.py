d7654033bc02cd7625f615b348d8b17bc51ed8d5
rishi163955 <rishi.s@mastek.com>
2026-06-01 19:41:06 +0530

    Integrate Snowflake fruit options and display search value
    Added functionality to display fruit options from Snowflake and show search value for chosen fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 from streamlit.errors import StreamlitSecretNotFoundError
 from snowflake.snowpark.functions import col
 from pathlib import Path
-import requests  
+import requests
+import pandas as pd
 
 try:
     import tomllib
@@ -57,6 +58,13 @@ fruit_list = [row["FRUIT_NAME"] for row in fruit_df.collect()]
 name_on_order = st.text_input("Name on smoothie:")
 st.write("Name on your smoothie will be:")
 st.write(name_on_order)
+
+my_df = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
+pd_df = my_df.to_pandas()
+st.dataframe(pd_df)
+# st.stop()
+search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
 ingredients_list = st.multiselect(
     "Choose up to 5 fruits",
